0a065b99aad2508bb9051b7ae5ae0f7e49aa10f9
mramadanf <mramadanf@banquemisr.com>
2020-04-26 16:16:23 +0200

    add

---
+++ NYTimes/ArticlesListModule/ViewModel/ArticlesViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ArticlesViewModel.swift
+//  NYTimes
+//
+//  Created by Mohamed Ramadan on 4/26/20.
+//  Copyright © 2020 Mohamed Ramadan. All rights reserved.
+//
+
+import Foundation
+
+class ArticleViewModel{
+    
+    func getArticlesList(numOfDays: Days, completion: @escaping (_ list: [ArticlesList]) -> Void){
+        let endPoint = EndPoints.getArticlesList(numOfDays: .one)
+        ApiClient.CallApi(endPoint: endPoint) { (result: Articles?, error: Error?, code) in
+            print(result)
+            guard let resultList = result?.results else { return }
+            completion(resultList)
+        }
+    }
+}
